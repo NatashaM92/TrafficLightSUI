71aae1b9f0e0f695f012eb840f7c98cffc7aecf1
Natalia <nata22808@mail.ru>
2023-07-18 20:00:40 +0300

    Initial Commit

---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Наталья Миронова on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
